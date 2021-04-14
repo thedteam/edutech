80b839dec74c320dffdb309d870db7cbc9f65d1e
thedteam <thedteam18@gmail.com>
2021-04-14 19:36:31 +0530

    m

--- assets/mobirise/js/custom.js
+++ assets/mobirise/js/custom.js
@@ -11,7 +11,7 @@ $(function () {
 
 	setTimeout(function () {
 		$('.loader_bg').fadeToggle();
-	}, 2000);
+	}, 4000);
 
 	/* JQuery Menu
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
